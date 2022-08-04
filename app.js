52c721f2cebe40b7c08f09d57a8c023b4b43f10e
Sam Koh <sam910615@gmail.com>
2022-08-04 11:38:19 +0800

    Update app.listen

--- app.js
+++ app.js
@@ -102,6 +102,10 @@ app.post("/submit/:postId", function (req, res) {
     res.redirect("/");
 });
 
-app.listen(3000, function () {
-    console.log("Server started on port 3000");
+// app.listen(3000, function () {
+//     console.log("Server started on port 3000");
+// });
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
